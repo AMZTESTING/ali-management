12674f416725df58d30b792f96549dbced10c9b2
unknown <hb2hw8hptq@privaterelay.appleid.com>
2026-08-21 15:48:48 +0400

    footer one

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -7,11 +7,16 @@ export default function MainLayout() {
   const [sidebarOpen, setSidebarOpen] = useState(false)
 
   return (
-    <div className="flex h-screen bg-gray-100 overflow-hidden">
-      <Sidebar open={sidebarOpen} onClose={() => setSidebarOpen(false)} />
-      <div className="flex-1 flex flex-col overflow-hidden min-w-0">
+    <div className="app-shell flex bg-gray-100 overflow-hidden">
+      <Sidebar
+        open={sidebarOpen}
+        onClose={() => setSidebarOpen(false)}
+      />
+
+      <div className="flex-1 flex flex-col overflow-hidden min-w-0 min-h-0">
         <Header onMenuClick={() => setSidebarOpen(true)} />
-        <main className="flex-1 overflow-y-auto overflow-x-hidden p-4 md:p-6">
+
+        <main className="app-main flex-1 min-h-0 overflow-y-auto overflow-x-hidden p-4 md:p-6">
           <Outlet />
         </main>
       </div>
